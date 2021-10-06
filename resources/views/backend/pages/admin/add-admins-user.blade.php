8326f81f1feaf726c0b5c9e9a8fbd1e89a2b20f5
Firoj <mymindhungry@gmail.com>
2021-10-06 15:16:23 +0545

    Add adminUsers And ShowAdminUser adding

--- resources/views/backend/pages/admin/add-admins-user.blade.php
+++ resources/views/backend/pages/admin/add-admins-user.blade.php
@@ -1,5 +1,67 @@
 @extends('backend.master.master')
 @section('content')
 
+    <body>
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper">
+        </div>
+    </div>
+
+
+    <!-- contain -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <div class="row">
+
+                    <div class="col-12 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">ADD USER</h4>
+                                <p class="card-description">
+                                    Welcome To Show users page.<code>{{date('D M y')}}</code>
+                                </p>
+                                <form class="forms-sample">
+                                    <div class="form-group">
+                                        <label for="exampleInputName1">Name</label>
+                                        <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail3">Username</label>
+                                        <input type="username" class="form-control" id="exampleInputusername" placeholder="Username">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail3">Email</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail" placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword4">Password</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword4">Confirm Password</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Confirm Password">
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <label>File upload</label>
+                                        <input type="file" name="img[]" class="file-upload-default">
+                                        <div class="input-group col-xs-12">
+                                            <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+                                            <span class="input-group-append">
+                          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+                        </span>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary mr-2">Submit</button>
+                                    <button class="btn btn-light">Cancel</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- end contain -->
+
 
 @endsection()
--- resources/views/backend/pages/admin/show-admins-users.blade.php
+++ resources/views/backend/pages/admin/show-admins-users.blade.php
@@ -1,3 +1,89 @@
 @extends('backend.master.master')
 @section('content')
+
+
+
+    <body>
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper">
+        </div>
+    </div>
+
+    <!-- contain -->
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <div class="row">
+
+                <div class="col-lg-12 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">SHOW USERS</h4>
+                            <p class="card-description">
+                                Welcome To Show users page.<code>{{date('D M y')}}</code>
+                            </p>
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>
+                                            S.N
+                                        </th>
+                                        <th>
+                                            Name
+                                        </th>
+                                        <th>
+                                            UserName
+                                        </th>
+                                        <th>
+                                            Email
+                                        </th>
+                                        <th>
+                                            User Types
+                                        </th>
+                                        <th>
+                                            Status
+                                        </th>
+                                        <th>
+                                            Images
+                                        </th>
+                                        <th>
+                                            Action
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <td>
+                                        01.
+                                    </td>
+                                    <td>
+                                        Herman Beck
+                                    </td>
+                                    <td>
+                                        $ 77.99
+                                    </td>
+                                    <td>
+                                        $ 77.99
+                                    </td>
+                                    <td>
+                                        $ 77.99
+                                    </td>
+                                    <td>
+                                        $ 77.99
+                                    </td>
+                                    <td class="py-1">
+                                        <img src="../../images/faces/face1.jpg" alt="image"/>
+                                    </td>
+                                    <td>
+                                        May 15, 2015
+                                    </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content ends -->
 @endsection()
