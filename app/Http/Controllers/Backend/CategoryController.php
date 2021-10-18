c870b606e0ce633d0ee78e29e9c3a1158f9412cf
Firoj <mymindhungry@gmail.com>
2021-10-18 16:47:09 +0545

    inserting slugify

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Category\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends BackendController
 {
@@ -24,12 +25,22 @@ class CategoryController extends BackendController
 
     public function add(Request $request)
     {
-       
+
         if ($request->isMethod('get')) {
             return view($this->pagePath . '.category.add-category', $this->data);
 
         }
         if ($request->isMethod('post')) {
+            $catObj = new Category();
+            $catObj->cat_name = $request->cat_name;
+            $catObj->slug = $this->slugGenerator($request->slug);
+            $catObj->meta_keywords = $request->mata_keywords;
+            $catObj->meta_description = $request->mata_description;
+            $catObj->description = $request->description;
+            $catObj->posted_by = Auth::guard('admin')->user()->id;
+            if ($catObj->save()) {
+                return redirect()->route('category')->with('success', 'Data was successfully inserted');
+            }
 
         }
     }
